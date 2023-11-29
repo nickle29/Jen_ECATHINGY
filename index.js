1f960a96f496afd7ffc16a7440954686266b2a89
nickle29 <144731872+nickle29@users.noreply.github.com>
2023-11-29 13:42:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let nameOfElement;
+nameOfElement = "demo";
+console.log("Books are life fr fr");
+
 // Código en español
 alert("heyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy");
 
@@ -6,4 +10,9 @@ console.log("heyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy");
 
 function changeText(){
   document.getElementById("demo").innerHTML = "If you like your coffee hot...";
+}
+
+function printToConsole(input) {
+window.alert(input);
+
 }
