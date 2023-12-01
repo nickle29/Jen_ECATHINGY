f7d40b248af26a26bede7156ccf166e34009f16c
nickle29 <144731872+nickle29@users.noreply.github.com>
2023-12-01 14:10:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ function changeText(){
   document.getElementById("demo").innerHTML = "If you like your coffee hot...";
 }
 
+let person= {
+firstName: "Nickle",
+  age: 69420,
+  favColor: "red",
+
+};
+
+function assignment(){
+  person.age ++;
+  window.alert(person.age);
+}
+
 function printToConsole(input) {
 window.alert(input);
 
